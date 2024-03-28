82a1f86bb3285196f50254e27cc60d400f629ebd
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-03-28 11:57:43 -0500

    agregando estructura del proyecto

---
+++ turistas/src/main/java/com/ugoback/turistas/TuristasApplication.java
@@ -0,0 +1,13 @@
+package com.ugoback.turistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristasApplication.class, args);
+	}
+
+}
---
+++ turistas/src/test/java/com/ugoback/turistas/TuristasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugoback.turistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
