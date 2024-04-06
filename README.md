1547d276459b5c9782d0c9b8af4d6d070e1c7a92
Damian Fanaro <damianfanaro@gmail.com>
2024-04-06 13:32:47 +0200

    Refactoring + properties

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Ugo
 app para viajeros
+
+# Spring
+
+Activate a specific Spring profile:
+
+    
--- src/test/java/com/ugo/TuristasApplicationTests.java
+++ src/test/java/com/ugo/TuristasApplicationTests.java
@@ -1,4 +1,4 @@
-package ugo.Turista;
+package com.ugo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
