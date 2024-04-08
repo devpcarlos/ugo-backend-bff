58cb79eee6f20b8c49c62bf2867a6302555a920e
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-08 16:33:17 -0500

    se agrega el metodo buscar por noombre

--- src/main/java/com/ugo/repository/UsersRepository.java
+++ src/main/java/com/ugo/repository/UsersRepository.java
@@ -4,4 +4,5 @@ import com.ugo.entitys.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    Users findByNombre(String nombre);
 }
