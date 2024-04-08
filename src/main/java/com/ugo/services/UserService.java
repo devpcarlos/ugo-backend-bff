6390ebb9d9ae30962cde7911a0bef07a959cfa75
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-08 16:34:27 -0500

    se agrega los servicios faltantes para terminar los endpoint

--- src/main/java/com/ugo/services/UserService.java
+++ src/main/java/com/ugo/services/UserService.java
@@ -2,8 +2,17 @@ package com.ugo.services;
 
 import com.ugo.entitys.Users;
 import com.ugo.repository.UsersRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -11,7 +20,62 @@ public class UserService {
     @Autowired
     private UsersRepository urs;
 
-    public void saveUser(Users user){
+    public ResponseEntity<?> saveUser(@Valid Users user, BindingResult result){
+        if(result.hasErrors()){
+            List<String> errorMessage = result.getAllErrors()
+                    .stream()
+                    .map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
         urs.save(user);
+        return ResponseEntity.ok("El usuario fue guardado");
     }
+
+    public List<Users> getAll(){
+        List<Users> lista= urs.findAll();
+        if (lista.isEmpty()){
+            throw new RuntimeException("No se encontraron usuarios en la base de datos");
+        }
+        return lista;
+    }
+
+    public ResponseEntity<?> getName(String nombre){
+        Users user= urs.findByNombre(nombre);
+        if (user==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El usuario con el nombre proporcionado no existe");
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    public ResponseEntity<?> updateUser( Long id, @Valid Users user,  BindingResult result) {
+        if (result.hasErrors()) {
+            List<String> errorMessage = result.getAllErrors()
+                    .stream()
+                    .map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+
+        Optional<Users> data = urs.findById(id);
+        if (!data.isPresent()) {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El usuario con el ID proporcionado no existe");
+
+        }
+        user.setId(id);
+        urs.save(user);
+        return ResponseEntity.ok("El usuario fue actualizado");
+
+    }
+
+    public ResponseEntity<?> deleteById(Long Id){
+        if (urs.existsById(Id)){
+            urs.deleteById(Id);
+            return ResponseEntity.ok("El usuario fue eliminado");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El usuario con el ID proporcionado no existe");
+        }
+    }
+
 }
