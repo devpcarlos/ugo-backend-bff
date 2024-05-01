d6c18cf23e3cbf3ab7c5f8390700a25e32b3bdc3
Damián Fanaro <damianfanaro@gmail.com>
2024-05-01 20:59:55 +0200

    Add filtering by category

--- src/main/java/com/ugo/controller/ExperienceController.java
+++ src/main/java/com/ugo/controller/ExperienceController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -43,8 +40,11 @@ public class ExperienceController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getExperiences() {
-        String baseUrl = dataApiUrl + ":" + dataApiPort + "/experience/";
+    public ResponseEntity<?> getExperiences(@RequestParam(required = false) String category) {
+        String baseUrl = dataApiUrl + ":" + dataApiPort + "/experience";
+        if (category != null) {
+            baseUrl += "?category=" + category;
+        }
         ResponseEntity<Experience[]> experiences = restTemplate.getForEntity(baseUrl, Experience[].class);
         return ResponseEntity.ok(experiences.getBody());
     }
