0f8e8b6d645fe7f244fbe4e95d8f4ec27cc721db
Damián Fanaro <damianfanaro@gmail.com>
2024-05-01 19:41:04 +0200

    Bug fix for border case and logging

--- src/main/java/com/ugo/controller/ExperienceController.java
+++ src/main/java/com/ugo/controller/ExperienceController.java
@@ -1,18 +1,24 @@
 package com.ugo.controller;
 
 import com.ugo.entitys.external.Experience;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(path = "/experience")
 public class ExperienceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExperienceController.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -26,8 +32,13 @@ public class ExperienceController {
     @GetMapping("{experienceId}")
     public ResponseEntity<?> getExperienceDetails(@PathVariable String experienceId) {
         String baseUrl = dataApiUrl + ":" + dataApiPort + "/experience/";
-        Experience experience = restTemplate.getForObject(baseUrl + experienceId, Experience.class);
-        return ResponseEntity.ok(experience);
+        try {
+            Experience experience = restTemplate.getForObject(baseUrl + experienceId, Experience.class);
+            return ResponseEntity.ok(experience);
+        } catch (HttpClientErrorException e) {
+            logger.error("Se solicitó una experiencia que no existe: {}", experienceId);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró la experiencia solicitada.");
     }
 
     @GetMapping
