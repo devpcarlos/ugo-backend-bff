40ce1f2551b97502558946cf0410c29e1c2d0265
Damián Fanaro <damianfanaro@gmail.com>
2024-05-01 21:21:36 +0200

    Fix reserve controller

--- src/main/java/com/ugo/services/IReserveService.java
+++ src/main/java/com/ugo/services/IReserveService.java
@@ -1,13 +1,20 @@
 package com.ugo.services;
-import com.ugo.dto.reserveDetailsDto;
+
+import com.ugo.dto.ReserveDetailsDto;
 import com.ugo.dto.reserveRequestDto;
-import jakarta.servlet.http.HttpServletRequest;
+
 import java.util.List;
+
 public interface IReserveService {
-    List<reserveRequestDto> FindAll();
-    void Save(reserveDetailsDto reserveDTO, HttpServletRequest request);
+
+    List<reserveRequestDto> findAll();
+
+    void save(ReserveDetailsDto reserveDTO);
+
     reserveRequestDto FindById(Long Id);
-    void Update(Long Id,reserveDetailsDto reserveDTO);
+
+    void Update(Long Id, ReserveDetailsDto reserveDTO);
+
     void DeleteById(Long id);
 
 }
--- src/main/java/com/ugo/services/ReserveService.java
+++ src/main/java/com/ugo/services/ReserveService.java
@@ -1,29 +1,34 @@
 package com.ugo.services;
 
-import com.ugo.dto.reserveDetailsDto;
+import com.ugo.dto.ReserveDetailsDto;
 import com.ugo.dto.reserveRequestDto;
 import com.ugo.entitys.Reserve;
 import com.ugo.entitys.State;
 import com.ugo.entitys.User;
 import com.ugo.exceptions.ReserveException;
 import com.ugo.repository.IReserveRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class ReserveService implements IReserveService {
+
     @Autowired
-    UserService US;
+    UserService userService;
+
     @Autowired
-    IReserveRepository ReserveRepo;
+    IReserveRepository reserveRepository;
+
     @Autowired
-    StateServiceImpl SS;
+    StateServiceImpl stateService;
+
     @Override
-    public List<reserveRequestDto>FindAll(){
-        List<reserveRequestDto>ReserveDtoList = ReserveRepo.findAll()
+    public List<reserveRequestDto> findAll() {
+        List<reserveRequestDto> ReserveDtoList = reserveRepository.findAll()
                 .stream()
                 .map(Reserve -> reserveRequestDto.builder()
                         .reserveDetails(Reserve.getReserveDetails())
@@ -33,58 +38,60 @@ public class ReserveService implements IReserveService {
                         .experience(Reserve.getExperience())
                         .build())
                 .toList();
-                return ReserveDtoList;
+        return ReserveDtoList;
     }
-    @Override
-    public reserveRequestDto FindById(Long Id){
-         Optional<Reserve>ReserveRequestDto = ReserveRepo.findById(Id);
-         if (ReserveRequestDto.isPresent()){
-             Reserve reserve = ReserveRequestDto.get();
-             reserveRequestDto reserveRequest = reserveRequestDto.builder()
-                     .duration(reserve.getDuration())
-                     .pax(reserve.getPax())
-                     .currency(reserve.getCurrency())
-                     .experience(reserve.getExperience())
-                     .build();
-             return reserveRequest;
-         }
-         else {
-             throw new ReserveException(500,"error");
-         }
-    }
-    @Override
-    public void Save(reserveDetailsDto reserveDTO, HttpServletRequest request){
-        State state = SS.findById(reserveDTO.getState());
-        User user = US.findById(reserveDTO.getReserveOwner());
-        if(
-                reserveDTO.getCurrency().isBlank() &&
-                        reserveDTO.getReserveOwner()==null &&
-                        reserveDTO.getState()==null &&
-                        reserveDTO.getDuration()==0
-        ){throw new ReserveException(400,"Incorrect status");}
-            else {
 
-                ReserveRepo.save(Reserve.builder()
-                            .Duration(reserveDTO.getDuration())
-                            .CreationDate(new Date(System.currentTimeMillis()))
-                            .UpdateDate(new Date(System.currentTimeMillis()))
-                            .ReserveOwner(user)
-                            .state(state)
-                            .floor(reserveDTO.getFloor())
-                            .Currency(reserveDTO.getCurrency())
-                            .experience(reserveDTO.getExperience())
-                    .build()
-                );
-            }
-        }
     @Override
-    public void Update(Long Id,reserveDetailsDto reserveDTO){
-        State state = SS.findById(reserveDTO.getState());
-        User user = US.findById(reserveDTO.getReserveOwner());
-        Optional<Reserve>reserveOptional=ReserveRepo.findById(Id);
-        if(reserveOptional.isPresent())
-        {
-            ReserveRepo.save(Reserve.builder()
+    public reserveRequestDto FindById(Long Id) {
+        Optional<Reserve> ReserveRequestDto = reserveRepository.findById(Id);
+        if (ReserveRequestDto.isPresent()) {
+            Reserve reserve = ReserveRequestDto.get();
+            reserveRequestDto reserveRequest = reserveRequestDto.builder()
+                    .duration(reserve.getDuration())
+                    .pax(reserve.getPax())
+                    .currency(reserve.getCurrency())
+                    .experience(reserve.getExperience())
+                    .build();
+            return reserveRequest;
+        } else {
+            throw new ReserveException(500, "error");
+        }
+    }
+
+    @Override
+    public void save(ReserveDetailsDto reserveDTO) {
+        State state = stateService.findById(reserveDTO.getState());
+        User user = userService.findById(reserveDTO.getReserveOwner());
+        if (
+                reserveDTO.getCurrency().isBlank() &&
+                        reserveDTO.getReserveOwner() == null &&
+                        reserveDTO.getState() == null &&
+                        reserveDTO.getDuration() == 0
+        ) {
+            throw new ReserveException(400, "Incorrect status");
+        } else {
+
+            reserveRepository.save(Reserve.builder()
+                    .Duration(reserveDTO.getDuration())
+                    .CreationDate(new Date(System.currentTimeMillis()))
+                    .UpdateDate(new Date(System.currentTimeMillis()))
+                    .ReserveOwner(user)
+                    .state(state)
+                    .floor(reserveDTO.getFloor())
+                    .Currency(reserveDTO.getCurrency())
+                    .experience(reserveDTO.getExperience())
+                    .build()
+            );
+        }
+    }
+
+    @Override
+    public void Update(Long Id, ReserveDetailsDto reserveDTO) {
+        State state = stateService.findById(reserveDTO.getState());
+        User user = userService.findById(reserveDTO.getReserveOwner());
+        Optional<Reserve> reserveOptional = reserveRepository.findById(Id);
+        if (reserveOptional.isPresent()) {
+            reserveRepository.save(Reserve.builder()
                     .Duration(reserveDTO.getDuration())
                     .CreationDate(new Date(System.currentTimeMillis()))
                     .UpdateDate(new Date(System.currentTimeMillis()))
@@ -94,17 +101,18 @@ public class ReserveService implements IReserveService {
                     .Currency(reserveDTO.getCurrency())
                     .experience(reserveDTO.getExperience())
                     .build());
-            }
-        else {
-            throw new ReserveException(404,"Update unaccomplished");
+        } else {
+            throw new ReserveException(404, "Update unaccomplished");
         }
 
     }
+
     @Override
-    public void DeleteById(Long Id){
-        if(Id!=null){ReserveRepo.deleteById(Id);}
-        else {
-            throw new ReserveException(404,"Incorrect Id");
+    public void DeleteById(Long Id) {
+        if (Id != null) {
+            reserveRepository.deleteById(Id);
+        } else {
+            throw new ReserveException(404, "Incorrect Id");
         }
     }
 }
